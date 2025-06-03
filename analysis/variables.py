72187a6f699f426b0f478f2fa875ff5053a3c495
piezga <piezga@gmail.com>
2025-06-03 18:59:55 +0200

    lots of things

--- analysis/variables.py
+++ analysis/variables.py
@@ -1,18 +1,18 @@
 
 #senm parameters
 
-nx = 512
-ny = 512
+nx = 500
+ny = 500
 kernel = '5'
 nu = '9.000000e-05'
 num_realizations = 100
 
-chosen_kernels= ['1','2','5','10']
+chosen_kernels= ['2','3','5']
 
 #other senm variables
 
-spatial_file_template = 'AConf_NX{nx}_NY{ny}_NU{nu}_Square_Kernsize{kernel}_rel.{realization}'
-abundance_file_template = 'Abund_NX{nx}_NY{ny}_NU{nu}_Square_Kernsize{kernel}_rel.'
+senm_spatial_file_template = 'AConf_NX{nx}_NY{ny}_NU{nu}_Square_Kernsize{kernel}_rel.{realization}'
+senm_abundance_file_template = 'Abund_NX{nx}_NY{ny}_NU{nu}_Square_Kernsize{kernel}_rel.{realization}'
 
 
 simulations_path = '../senm/'
@@ -23,4 +23,5 @@ forests = ['barro', 'wanang']
 
 path_template = '../data/{forest}/'
 census_template = '{forest}_{census}.csv'
+abundance_template = '{forest}_abundances_{census}.csv'
 names_template = 'names_{forest}_{census}.txt'
